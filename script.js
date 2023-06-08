475f4267dd974c1a13448b80449535398c124a08
deltanoru <cleanest@internet.ru>
2023-06-08 22:09:59 +0300

    Complete the js
    It still does lag a little bit when clicking mouse button a lot of times
    But I have no idea how to fix it so let it be..

--- script.js
+++ script.js
@@ -1,21 +1,76 @@
-function changeScreenSize () {
-    let new_size = +slider.value;
-    grid.style.gridTemplateColumns = `repeat(${new_size}, 1fr)`;
-    grid.style.gridTemplateRows = `repeat(${new_size}, 1fr)`;
-    size_placeholder.textContent = `${new_size} x ${new_size}`;
+function clearGrid () {
+    grid.innerHTML = ""
 }
 
 
-let current_tool = 'pencil';
-const color_picker = document.getElementById('color-picker');
+function setGridSize () {
+    gridSize = +slider.value;
+    grid.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${gridSize}, 1fr)`;
+    sizePlaceholder.textContent = `${gridSize} x ${gridSize}`;
+}
+
+
+function fillGrid () {
+    for (let i = 0; i < gridSize * gridSize; i++) {
+        let grid_child = document.createElement('div');
+        grid_child.classList.add('grid-child');
+        grid.appendChild(grid_child);
+    }
+    gridElements = grid.querySelectorAll('.grid-child');
+}
+
+
+function createGrid () {
+    clearGrid();
+    setGridSize();
+    fillGrid();
+    addGridElementsListeners();
+}
+
+
+function addGridElementsListeners () {
+    [...gridElements].forEach(el => el.addEventListener('mouseover', colorGridElement));
+    [...gridElements].forEach(el => el.addEventListener('mousedown', colorGridElement));
+}
+
+
+function colorGridElement (e) {
+    if (e.type === 'mouseover' && !mouseDown) return
+    if (mouseDown){
+        if (instrument == 'pencil'){
+            e.target.style.backgroundColor = color;
+        } else {
+            e.target.style.backgroundColor = '#ffffff'
+        }
+    }
+}
+
+
+function changeColor () {
+    color = colorPicker.value;
+}
+
+
+let color = "#000000"
+let mouseDown = false;
+let gridSize = 16;
+let gridElements;
+let instrument = 'pencil';
+
+const colorPicker = document.getElementById('color-picker');
 const slider = document.getElementById('slider');
 const grid = document.getElementById('grid');
 const pencil = document.getElementById('pencil');
 const eraser = document.getElementById('eraser');
 const redo = document.getElementById('redo');
-const size_placeholder = document.getElementById('size');
+const sizePlaceholder = document.getElementById('size');
+createGrid(); // grid first setup
 
-slider.addEventListener('change', changeScreenSize);
-
-
-// TODO: grid-element: addEventListener('mouseDown', draw) + btns
+document.body.onmousedown = () => mouseDown = true;
+document.body.onmouseup = () => mouseDown = false;
+colorPicker.onchange = changeColor;
+slider.onchange = createGrid;
+pencil.onclick = () => instrument = 'pencil';
+eraser.onclick = () => instrument = 'eraser';
+redo.onclick = createGrid;
